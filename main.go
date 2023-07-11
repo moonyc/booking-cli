cb6b9de2ac093f454f09723ca9c93e93197bc678
muni <booscarface@gmail.com>
2023-07-11 14:51:19 +0200

    slices: abstraction of arrays

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Printf("We have a total of %v and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var bookings [50]string
+	var bookings []string
 
 	var firstName string
 	var lastName string
