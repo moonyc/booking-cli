f12e3fb30281ad61cf7157d1779d7c7f991bf240
muni <booscarface@gmail.com>
2023-07-11 13:41:51 +0200

    go is statically typed

--- main.go
+++ main.go
@@ -11,4 +11,8 @@ func main() {
 	fmt.Printf("We have a total of %v and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
+	var userName string
+
+	userName = "Tom"
+	fmt.Println(userName)
 }
