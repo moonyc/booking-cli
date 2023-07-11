8cfa753d38db40d02f48ce9ecce97b4b8462231c
muni <booscarface@gmail.com>
2023-07-11 13:29:46 +0200

    const vs var

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Armadillo Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
 	fmt.Println("Welcome to our", conferenceName, "booking application!")
+	fmt.Println("We have a total of", conferenceTickets, "and ", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend.")
 
 }
